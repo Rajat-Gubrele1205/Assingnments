5be0b16f3eac0a3021daa18331ff8464d133882c
Rajat Gubrele <rajat@inito.com>
2023-07-17 15:33:58 +0530

    Second Commit

--- sol1.sh
+++ sol1.sh
@@ -2,7 +2,7 @@
 
 echo "Enter file name : "
 read file_name
-cut -d " " -f 9 $file_name | grep "200" | wc -l
-cut -d " " -f 9 $file_name
+awk '{print $9}' $file_name | grep "200" | wc -l
+awk '{ print $9 }' $file_name
 echo "Total no. of log responses :"
 wc -l $file_name
