a36dd9e660c1813d0c9c406c93e9ea075874fbfc
Rajat Gubrele <rajat@inito.com>
2023-07-17 19:04:05 +0530

    First Commit

---
+++ sol2.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+echo "Enter command for child process : "
+read command
+child_process(){
+	$command
+	pid=$!
+}
+monitor(){
+while true; do
+if ! ps -p $pid 2> /dev/null; then
+
+	child_process
+
+fi
+sleep 10
+done 
+}
+if [ $# -eq 0 ]; then 
+	monitor
+	
+fi
